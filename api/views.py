1ab53b9eb0f5685813c03e1bd5ffb3554c437b1b
Rakesh Sangem <rakeshsangem88@gmail.com>
2025-07-20 22:25:41 +0530

    add productsinfo with custom serializers

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from api.serializers import ProductSerializer, OrderSerializer
+from django.db.models import Max
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -23,4 +24,16 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        "products": products,
+        "count": len(products),
+        "max_price": products.aggregate(max_price=Max('price'))['max_price']
+    })
+
     return Response(serializer.data)
