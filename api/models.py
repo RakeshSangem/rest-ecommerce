f72d4ddef4baa80cb4fa2f6fc53a2a7a4a52ded8
Rakesh Sangem <rakeshsangem88@gmail.com>
2025-07-20 21:19:39 +0530

    initial commit 🚀

---
+++ api/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+	"""Custom user model extending Django's AbstractUser"""
+	pass
+
+class Product(models.Model):
+	"""Model representing a product in the store."""
+
+	name = models.CharField(max_length=255)
+	description = models.TextField()	
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	stock = models.PositiveBigIntegerField()
+	image = models.ImageField(upload_to='products/', blank=True, null=True)
+	
+	@property
+	def in_stock(self):
+		return self.stock > 0
+	
+	def __str__(self):
+		return self.name
+
+class Order(models.Model):
+	"""Model representing an order in the store."""
+
+	from django.db import models
+
+	class StatusChoices(models.TextChoices):
+		PENDING = 'pending', 'Pending'
+		SHIPPED = 'shipped', 'Shipped'
+		DELIVERED = 'delivered', 'Delivered'
+		CANCELLED = 'cancelled', 'Cancelled'
+
+	order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	status = models.CharField(max_length=10, choices=StatusChoices.choices, default=StatusChoices.PENDING)
+	products = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
+
+class OrderItem(models.Model):
+	"""Model representing an item in an order."""
+
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	quantity = models.PositiveIntegerField()
+
+	@property
+	def item_subtotal(self):
+		return self.product.price * self.quantity
+
+	def __str__(self):
+		return f"{self.quantity} x {self.product.name} (Order: {self.order.order_id})"
