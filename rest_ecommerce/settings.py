cc7a67baaeb2be83bd26ee709830376ef1e492a4
Rakesh Sangem <rakeshsangem88@gmail.com>
2025-07-21 21:22:40 +0530

    feat: add docs support

--- rest_ecommerce/settings.py
+++ rest_ecommerce/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'silk',
-
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -123,5 +123,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Ecommerce API',
+    'DESCRIPTION': 'Ecommerce API for managing products and orders',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
